c755a0dc1e5761f2d3421d4e9bc731369ba2b024
Dale-Harrison <dailofernandez@gmail.com>
2012-01-02 21:28:17 +0000

    slight issue in default args fixed

--- src/valurl/core.clj
+++ src/valurl/core.clj
@@ -7,5 +7,5 @@
   (with-command-line
     args
     "Usage: -url <FQDN URL> e.g. -url \"http://www.slashdot.org\""
-    [[url "The URL to be processed" "http://www.slashdot.co.uk"]]
+    [[url "The URL to be processed" "http://www.google.co.uk"]]
     (process-links (get-links url))))
